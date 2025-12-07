7e89348c967005625bfbdf6bd388970a392bf228
blacklioncorp <rodriguezianmerzi@gmail.com>
2025-12-07 14:11:31 -0600

    fix: Habilitar CORS para el dominio de Vercel

--- app/main.py
+++ app/main.py
@@ -18,7 +18,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["http://localhost:5173",
+                   "https://elrefugioposfroontend.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
